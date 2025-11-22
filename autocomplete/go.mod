499372b67e5f80be40ef4b9a50e6246ea53c0bf1
chonlaphoom <chonlaphoomth@gmail.com>
2025-11-22 22:42:48 +0700

    import jsonrpc2 structurer

---
+++ autocomplete/go.mod
@@ -0,0 +1,5 @@
+module autocomplete
+
+go 1.25.0
+
+require github.com/sourcegraph/jsonrpc2 v0.2.1 // indirect
---
+++ autocomplete/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sourcegraph/jsonrpc2"
+)
+
+const BUFFER_SIZE = 1 << 5
+
+func init() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		fmt.Println("Received interrupt signal, gracefully exiting...")
+		os.Exit(0)
+	}()
+}
+
+func main() {
+	buffer := make([]byte, BUFFER_SIZE)
+	for {
+		n, err := os.Stdin.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading from stdin:", err)
+			panic(err)
+		}
+
+		fmt.Println(string(buffer[:n]))
+	}
+
+	// http://www.jsonrpc.org/specification#response_object.
+	res := jsonrpc2.Response{
+		ID:    jsonrpc2.ID{Num: 1}, // should match the request ID
+		Error: nil,
+	}
+
+	res.SetResult(`
+			{
+				"jsonrpc": "2.0",
+				"id": 123,
+				"result": [
+					{
+						"label": "fmt.Println",
+						"kind": 2 // Function
+					},
+				]
+			}
+	`)
+
+	result, _ := json.Marshal(res)
+	fmt.Printf("%v", string(result))
+}
